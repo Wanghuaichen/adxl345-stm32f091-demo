effe5cfebca8c1fc62b3e4a7da297f6f62414be3
Vincent <Vincent.Shanghai@hotmail.com>
2018-06-08 23:33:31 +0800

    Initial Version, but it's working properly.

---
+++ adxl345_stm32f091/Inc/Backup/main.h.bak
@@ -0,0 +1,185 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define APP_DET_Pin GPIO_PIN_5
+#define APP_DET_GPIO_Port GPIOE
+#define LASER_CTRL_Pin GPIO_PIN_2
+#define LASER_CTRL_GPIO_Port GPIOC
+#define APT_B_Pin GPIO_PIN_0
+#define APT_B_GPIO_Port GPIOA
+#define BEEP_CTRL_Pin GPIO_PIN_1
+#define BEEP_CTRL_GPIO_Port GPIOA
+#define NTC_CTRL_Pin GPIO_PIN_3
+#define NTC_CTRL_GPIO_Port GPIOA
+#define LED_PWR_Pin GPIO_PIN_5
+#define LED_PWR_GPIO_Port GPIOA
+#define LED_HI_Pin GPIO_PIN_6
+#define LED_HI_GPIO_Port GPIOA
+#define LED_PUBLIC_Pin GPIO_PIN_7
+#define LED_PUBLIC_GPIO_Port GPIOA
+#define LED_PRIVATE_Pin GPIO_PIN_4
+#define LED_PRIVATE_GPIO_Port GPIOC
+#define LED_BLE_Pin GPIO_PIN_5
+#define LED_BLE_GPIO_Port GPIOC
+#define LED_RF_Pin GPIO_PIN_0
+#define LED_RF_GPIO_Port GPIOB
+#define LED_X_Pin GPIO_PIN_1
+#define LED_X_GPIO_Port GPIOB
+#define LED_Y_Pin GPIO_PIN_2
+#define LED_Y_GPIO_Port GPIOB
+#define LED_NORMAL_Pin GPIO_PIN_7
+#define LED_NORMAL_GPIO_Port GPIOE
+#define DC_MOTOR_NSLEEP_Pin GPIO_PIN_8
+#define DC_MOTOR_NSLEEP_GPIO_Port GPIOE
+#define LED_SLOPE_Pin GPIO_PIN_9
+#define LED_SLOPE_GPIO_Port GPIOE
+#define KEY_BLE_Pin GPIO_PIN_10
+#define KEY_BLE_GPIO_Port GPIOE
+#define KEY_RF_Pin GPIO_PIN_11
+#define KEY_RF_GPIO_Port GPIOE
+#define KEY_XY_Pin GPIO_PIN_12
+#define KEY_XY_GPIO_Port GPIOE
+#define ADXL345_INT2_Pin GPIO_PIN_13
+#define ADXL345_INT2_GPIO_Port GPIOE
+#define ADXL345_INT2_EXTI_IRQn EXTI4_15_IRQn
+#define ADXL345_INT1_Pin GPIO_PIN_14
+#define ADXL345_INT1_GPIO_Port GPIOE
+#define ADXL345_INT1_EXTI_IRQn EXTI4_15_IRQn
+#define WL_PWR_SW_Pin GPIO_PIN_15
+#define WL_PWR_SW_GPIO_Port GPIOE
+#define ADXL345_SCL_Pin GPIO_PIN_10
+#define ADXL345_SCL_GPIO_Port GPIOB
+#define ADXL345_SDA_Pin GPIO_PIN_11
+#define ADXL345_SDA_GPIO_Port GPIOB
+#define DIR_DET_A_Pin GPIO_PIN_12
+#define DIR_DET_A_GPIO_Port GPIOB
+#define DIR_DET_B_Pin GPIO_PIN_13
+#define DIR_DET_B_GPIO_Port GPIOB
+#define DIR_DET_C_Pin GPIO_PIN_14
+#define DIR_DET_C_GPIO_Port GPIOB
+#define LASER_EN_Pin GPIO_PIN_15
+#define LASER_EN_GPIO_Port GPIOB
+#define KEY_MODE_Pin GPIO_PIN_9
+#define KEY_MODE_GPIO_Port GPIOD
+#define KEY_ANGLE_Pin GPIO_PIN_10
+#define KEY_ANGLE_GPIO_Port GPIOD
+#define KEY_SPEED_Pin GPIO_PIN_11
+#define KEY_SPEED_GPIO_Port GPIOD
+#define KEY_DOWN_Pin GPIO_PIN_12
+#define KEY_DOWN_GPIO_Port GPIOD
+#define KEY_UP_Pin GPIO_PIN_13
+#define KEY_UP_GPIO_Port GPIOD
+#define STP_MOTOR_XA_Pin GPIO_PIN_6
+#define STP_MOTOR_XA_GPIO_Port GPIOC
+#define STP_MOTOR_XB_Pin GPIO_PIN_7
+#define STP_MOTOR_XB_GPIO_Port GPIOC
+#define STP_MOTOR_XC_Pin GPIO_PIN_8
+#define STP_MOTOR_XC_GPIO_Port GPIOC
+#define STP_MOTOR_XD_Pin GPIO_PIN_9
+#define STP_MOTOR_XD_GPIO_Port GPIOC
+#define STP_MOTOR_YA_Pin GPIO_PIN_8
+#define STP_MOTOR_YA_GPIO_Port GPIOA
+#define STP_MOTOR_YB_Pin GPIO_PIN_11
+#define STP_MOTOR_YB_GPIO_Port GPIOA
+#define STP_MOTOR_YC_Pin GPIO_PIN_12
+#define STP_MOTOR_YC_GPIO_Port GPIOA
+#define STP_MOTOR_YD_Pin GPIO_PIN_6
+#define STP_MOTOR_YD_GPIO_Port GPIOF
+#define DIR_DET_D_Pin GPIO_PIN_15
+#define DIR_DET_D_GPIO_Port GPIOA
+#define APT_C_Pin GPIO_PIN_1
+#define APT_C_GPIO_Port GPIOD
+#define APT_A_Pin GPIO_PIN_2
+#define APT_A_GPIO_Port GPIOD
+#define E2P_WP_Pin GPIO_PIN_3
+#define E2P_WP_GPIO_Port GPIOD
+#define SPI_FLASH_SCK_Pin GPIO_PIN_4
+#define SPI_FLASH_SCK_GPIO_Port GPIOD
+#define SPI_FLASH_MOSI_Pin GPIO_PIN_5
+#define SPI_FLASH_MOSI_GPIO_Port GPIOD
+#define SPI_FLASH_MISO_Pin GPIO_PIN_6
+#define SPI_FLASH_MISO_GPIO_Port GPIOD
+#define SPI_FLASH_NSS_Pin GPIO_PIN_7
+#define SPI_FLASH_NSS_GPIO_Port GPIOD
+#define KEY_PWR_Pin GPIO_PIN_3
+#define KEY_PWR_GPIO_Port GPIOB
+#define PWR_LOCK_Pin GPIO_PIN_4
+#define PWR_LOCK_GPIO_Port GPIOB
+#define IR_CAP_Pin GPIO_PIN_5
+#define IR_CAP_GPIO_Port GPIOB
+
+/* ########################## Assert Selection ############################## */
+/**
+  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
+  *        HAL drivers code
+  */
+ #define USE_FULL_ASSERT    1U 
+
+/* USER CODE BEGIN Private defines */
+
+#define LOCK_MAIN_POWER       HAL_GPIO_WritePin(PWR_LOCK_GPIO_Port, PWR_LOCK_Pin, GPIO_PIN_SET)
+#define RELEASE_MAIN_POWER    HAL_GPIO_WritePin(PWR_LOCK_GPIO_Port, PWR_LOCK_Pin, GPIO_PIN_RESET) 
+#define TOGGLE_POWER_LED      HAL_GPIO_TogglePin(LED_PWR_GPIO_Port, LED_PWR_Pin)
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+void _Error_Handler(char *, int);
+
+#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ adxl345_stm32f091/Src/main.c
@@ -0,0 +1,546 @@
+
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f0xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+
+#include "ADXL345.h"
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c2;
+
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_TIM2_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+void stdout_putchar(unsigned char c)
+{
+	HAL_UART_Transmit(&huart1, (uint8_t* )(&c), 1, 1000) ;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  int loop_counter = 0 ;
+  volatile int16_t x = 0 ;
+
+  volatile int16_t y = 0 ;
+
+  volatile int16_t z = 0 ;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C2_Init();
+  MX_USART1_UART_Init();
+  MX_TIM3_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+
+
+  LOCK_MAIN_POWER ;
+
+
+  ADXL345_Init() ;
+  
+  
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+    if(loop_counter % 100 == 0)
+    {
+      TOGGLE_POWER_LED ;
+      ADXL345_GetXyz(&x, &y, &z) ;
+    }
+    if(loop_counter++ > 10000) loop_counter = 0 ;
+  }
+  /* USER CODE END 3 */
+
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* I2C2 init function */
+static void MX_I2C2_Init(void)
+{
+
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x2010091A;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure Analogue filter 
+    */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure Digital filter 
+    */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* TIM2 init function */
+static void MX_TIM2_Init(void)
+{
+
+  TIM_ClockConfigTypeDef sClockSourceConfig;
+  TIM_MasterConfigTypeDef sMasterConfig;
+
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 299;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 8000;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* TIM3 init function */
+static void MX_TIM3_Init(void)
+{
+
+  TIM_ClockConfigTypeDef sClockSourceConfig;
+  TIM_MasterConfigTypeDef sMasterConfig;
+  TIM_IC_InitTypeDef sConfigIC;
+
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 19;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 4000;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  if (HAL_TIM_IC_Init(&htim3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_BOTHEDGE;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* USART1 init function */
+static void MX_USART1_UART_Init(void)
+{
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, LASER_CTRL_Pin|LED_PRIVATE_Pin|LED_BLE_Pin|STP_MOTOR_XA_Pin 
+                          |STP_MOTOR_XB_Pin|STP_MOTOR_XC_Pin|STP_MOTOR_XD_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, BEEP_CTRL_Pin|NTC_CTRL_Pin|LED_PWR_Pin|LED_HI_Pin 
+                          |LED_PUBLIC_Pin|STP_MOTOR_YA_Pin|STP_MOTOR_YB_Pin|STP_MOTOR_YC_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED_RF_Pin|LED_X_Pin|LED_Y_Pin|LASER_EN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, LED_NORMAL_Pin|DC_MOTOR_NSLEEP_Pin|LED_SLOPE_Pin|WL_PWR_SW_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(STP_MOTOR_YD_GPIO_Port, STP_MOTOR_YD_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, E2P_WP_Pin|SPI_FLASH_SCK_Pin|SPI_FLASH_NSS_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(SPI_FLASH_MOSI_GPIO_Port, SPI_FLASH_MOSI_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(PWR_LOCK_GPIO_Port, PWR_LOCK_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : APP_DET_Pin KEY_BLE_Pin KEY_RF_Pin KEY_XY_Pin */
+  GPIO_InitStruct.Pin = APP_DET_Pin|KEY_BLE_Pin|KEY_RF_Pin|KEY_XY_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LASER_CTRL_Pin LED_PRIVATE_Pin LED_BLE_Pin STP_MOTOR_XA_Pin 
+                           STP_MOTOR_XB_Pin STP_MOTOR_XC_Pin STP_MOTOR_XD_Pin */
+  GPIO_InitStruct.Pin = LASER_CTRL_Pin|LED_PRIVATE_Pin|LED_BLE_Pin|STP_MOTOR_XA_Pin 
+                          |STP_MOTOR_XB_Pin|STP_MOTOR_XC_Pin|STP_MOTOR_XD_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : APT_B_Pin */
+  GPIO_InitStruct.Pin = APT_B_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(APT_B_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BEEP_CTRL_Pin NTC_CTRL_Pin LED_PWR_Pin LED_HI_Pin 
+                           LED_PUBLIC_Pin STP_MOTOR_YA_Pin STP_MOTOR_YB_Pin STP_MOTOR_YC_Pin */
+  GPIO_InitStruct.Pin = BEEP_CTRL_Pin|NTC_CTRL_Pin|LED_PWR_Pin|LED_HI_Pin 
+                          |LED_PUBLIC_Pin|STP_MOTOR_YA_Pin|STP_MOTOR_YB_Pin|STP_MOTOR_YC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_RF_Pin LED_X_Pin LED_Y_Pin LASER_EN_Pin 
+                           PWR_LOCK_Pin */
+  GPIO_InitStruct.Pin = LED_RF_Pin|LED_X_Pin|LED_Y_Pin|LASER_EN_Pin 
+                          |PWR_LOCK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_NORMAL_Pin DC_MOTOR_NSLEEP_Pin LED_SLOPE_Pin WL_PWR_SW_Pin */
+  GPIO_InitStruct.Pin = LED_NORMAL_Pin|DC_MOTOR_NSLEEP_Pin|LED_SLOPE_Pin|WL_PWR_SW_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ADXL345_INT2_Pin ADXL345_INT1_Pin */
+  GPIO_InitStruct.Pin = ADXL345_INT2_Pin|ADXL345_INT1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DIR_DET_A_Pin DIR_DET_B_Pin DIR_DET_C_Pin KEY_PWR_Pin */
+  GPIO_InitStruct.Pin = DIR_DET_A_Pin|DIR_DET_B_Pin|DIR_DET_C_Pin|KEY_PWR_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : KEY_MODE_Pin KEY_ANGLE_Pin KEY_SPEED_Pin KEY_DOWN_Pin 
+                           KEY_UP_Pin */
+  GPIO_InitStruct.Pin = KEY_MODE_Pin|KEY_ANGLE_Pin|KEY_SPEED_Pin|KEY_DOWN_Pin 
+                          |KEY_UP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : STP_MOTOR_YD_Pin */
+  GPIO_InitStruct.Pin = STP_MOTOR_YD_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(STP_MOTOR_YD_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : DIR_DET_D_Pin */
+  GPIO_InitStruct.Pin = DIR_DET_D_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(DIR_DET_D_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : APT_C_Pin APT_A_Pin */
+  GPIO_InitStruct.Pin = APT_C_Pin|APT_A_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : E2P_WP_Pin */
+  GPIO_InitStruct.Pin = E2P_WP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(E2P_WP_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SPI_FLASH_SCK_Pin SPI_FLASH_MOSI_Pin SPI_FLASH_NSS_Pin */
+  GPIO_InitStruct.Pin = SPI_FLASH_SCK_Pin|SPI_FLASH_MOSI_Pin|SPI_FLASH_NSS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SPI_FLASH_MISO_Pin */
+  GPIO_InitStruct.Pin = SPI_FLASH_MISO_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(SPI_FLASH_MISO_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI4_15_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    
+  printf("Wrong parameters value: file %s on line %d\r\n", file, line) ;
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
